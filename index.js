d7c920e1d8659b043d7394cb3739e4c0aa9b917c
github username <github email address>
2019-05-11 18:19:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = parseInt(Math.random() * 10) + 1;
+
+const mod = 54 % 5;
+
+const max = Math.max(2, 3, 20);
