faaccbb78e5eadf8998f584ce1b79694f97de590
Daniel Hallada <hallada.daniel@gmail.com>
2019-05-12 12:58:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31
+
+const num2 = 2
+
+const multiply = num1 * num2
+
+const random = Math.random() 
+
+const mod = 12 % 8
+
+const max = Math.max(20, 4, 5, 15, 12.5)
